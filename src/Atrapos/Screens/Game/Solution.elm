61d595a391478c28e92f95dc332ed1a0fa199fea
Yury Martynov <yuri.martynov@gmail.com>
2016-12-12 00:12:26 +0200

    solution

--- src/Atrapos/Screens/Game/Solution.elm
+++ src/Atrapos/Screens/Game/Solution.elm
@@ -19,22 +19,33 @@ solve ({ nodes, links } as model) =
                 |> List.sortBy (\( _, ( _, _, len ) ) -> len)
                 |> List.map (\( id, ( n1, n2, _ ) ) -> ( id, n1, n2 ))
 
-        path n1 n2 edges =
-            case edges of
+        path n1 n2 edges  =
+            case edges |> List.sortBy (\(_,m1,_) -> if n1 == m1 then 0 else 1) of
                 [] -> False
                 ((id, m1, m2) as head)::tail -> 
-                    if (n1 == m1) then
-                        if (n2 == m2) then 
-                            True
+                    let path_ n1 n2 m1 m2  =
+                        if (n1 == m1) then
+                            if (n2 == m2) then 
+                                True
+                            else
+                                path m2 n2 tail 
                         else
-                            path m2 n2 tail
-                    else 
-                        path n1 n2 tail
+                            -- if acc |> List.member id then
+                                path n1 n2 tail 
+                            -- else
+                                -- path n1 n2 (tail ++ [head]) (id::acc)
+                            
+                    in
+                        path_ n1 n2 m1 m2 
+                        ||
+                        path_ n1 n2 m2 m1 
+                        ||
+                        path_ n2 n1 m1 m2 
                         -- ||
-                        -- path n2 n1 ((id, m2, m1) :: tail)
+                        -- path_ n2 n1 m2 m1 
 
         disconnected n1 n2 edges =
-            not <| path n1 n2 edges
+            not <| path n1 n2 edges 
                 
             -- case nodes of
             --     [] ->
