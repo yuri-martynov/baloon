38519d77c9310c624e0c07d608b4667f66b16dbc
Yuri <yuri.martynov@gmail.com>
2016-12-13 16:55:30 +0200

    1

--- src/Atrapos/Screens/Game/Objects/Link/View.elm
+++ src/Atrapos/Screens/Game/Objects/Link/View.elm
@@ -5,19 +5,49 @@ import Svg.Attributes exposing (..)
 import Svg.Events exposing (..)
 import Json.Decode exposing (succeed)
 import Common.Dict exposing (..)
-import Atrapos.Screens.Game.Model exposing (Link, Node, Model)
+import Atrapos.Screens.Game.Model exposing (Link, Node, Model, NodeId)
 import Atrapos.Screens.Game.Objects.Link.Msg exposing (..)
 
 
 view : Model -> Link -> Svg Msg
-view { nodes } { node1, node2, selected, len } =
+view { nodes } ({ node1, node2, selected, len } as link) =
     let
-        n1 = nodes # node1
-        n2 = nodes # node2 
+        n1 =
+            nodes # node1
+
+        n2 =
+            nodes # node2
+
+        class_ =
+            if selected then
+                "selected"
+            else
+                ""
+
+        id_ =
+            linkHash link |> toString
     in
-        [ line_ n1 n2 selected
-        , length len n1 n2
-        ] |> g []
+        [ line
+            [ id_ |> id
+            , n1.x |> toString |> x1
+            , n1.y |> toString |> y1
+            , n2.x |> toString |> x2
+            , n2.y |> toString |> y2
+            , onMouseDown Toggle
+              -- , on "touchstart" (succeed Toggle)
+            , "link " ++ class_ |> class
+            ]
+            []
+        , text_
+            [ (n1.x + n2.x) / 2 |> toString |> x
+            , (n1.y + n2.y) / 2 |> toString |> y
+            , class "len"
+            ]
+            [ [ len |> toString |> text ]
+                |> textPath [ linkHash link |> toString |> (++) "#" |> xlinkHref ]
+            ]
+        ]
+            |> g []
 
 
 stroke : Svg msg
@@ -32,31 +62,6 @@ stroke =
 -- PRIVATE ------------------
 
 
-line_ : Node -> Node -> Bool -> Svg Msg
-line_ n1 n2 selected =
-    let
-        class_ =
-            if selected then
-                "selected"
-            else
-                ""
-    in
-        line
-            [ n1.x |> toString |> x1
-            , n1.y |> toString |> y1
-            , n2.x |> toString |> x2
-            , n2.y |> toString |> y2
-            , onMouseDown Toggle
-              -- , on "touchstart" (succeed Toggle)
-            , "link " ++ class_ |> class
-            ]
-            []
-
-length: Float -> Node -> Node -> Svg Msg
-length len n1 n2 =
-    text_
-        [ (n1.x + n2.x) /2 |> toString |> x
-        , (n1.y + n2.y) /2 |> toString |> y 
-        , fontSize "2"
-        ]
-        [ len |> toString |> text ]
+linkHash : Link -> Int
+linkHash { node1, node2 } =
+    node1 * 100 + node2
