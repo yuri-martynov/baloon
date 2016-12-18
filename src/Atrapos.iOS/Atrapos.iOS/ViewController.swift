dca85713ff4316f6ab7830f5b7a6e70694f67495
Yury Martynov <yuri.martynov@gmail.com>
2016-12-18 19:26:55 +0200

    iOS

---
+++ src/Atrapos.iOS/Atrapos.iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Atrapos.iOS
+//
+//  Created by Yury Martynov on 18/12/2016.
+//  Copyright © 2016 Yury Martynov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
