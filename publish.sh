02705ace88702ffeb38fae4a35c96a4952a82111
Yury Martynov <yuri.martynov@gmail.com>
2016-12-30 17:56:07 +0200

    fix publish

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ cp src/atrapos/index.html docs
 cp src/atrapos/main.js docs
 cp src/atrapos/style.css docs
 cp src/atrapos/db/*.* docs/db
-cp src/atrapos/screens/game/node/*.svg docs/screens/game/node
+cp src/atrapos/game/node/*.svg docs/game/node
 
 # cp src/atrapos/main.js src/atraposCordova/www/js
 # cp src/atrapos/style.css src/atraposCordova/www/css
