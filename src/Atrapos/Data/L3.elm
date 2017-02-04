c1d2551ee5883a98d2a9bc557af950923b6561a5
Yury Martynov <yuri.martynov@gmail.com>
2017-02-04 20:41:41 +0200

    (add) l3

---
+++ src/Atrapos/Data/L3.elm
@@ -0,0 +1,26 @@
+module Atrapos.Data.L3 exposing (model)
+
+import Atrapos.Data.Model exposing (..)
+
+
+model : Model
+model =
+    { nodes =
+        [ ( 0, 15 )
+        , ( 1, 4 )
+        , ( 1, 10 )
+        , ( 5, 1 )
+        , ( 5, 19 )
+        , ( 9, 4 )
+        , ( 9, 10 )
+        , ( 10, 15 )
+        ]
+    , links =
+        [ ( 1,      [3, 5, 8    ])
+        , ( 2,      [4, 7       ])
+        , ( 3,      [4, 5, 6, 7 ])
+        , ( 4,      [5, 6, 7    ])
+        , ( 5,      [7, 8       ])
+        , ( 7,      [8          ])
+        ]
+    }
--- src/Atrapos/Data/Levels.elm
+++ src/Atrapos/Data/Levels.elm
@@ -4,12 +4,14 @@ import Dict exposing (Dict)
 import Atrapos.Data.Model exposing (Levels)
 import Atrapos.Data.L1 as L1
 import Atrapos.Data.L2 as L2
+import Atrapos.Data.L3 as L3
 
 
 model : Levels
 model =
     [ L1.model
     , L2.model
+    , L3.model
     ]
         |> List.indexedMap (,)
         |> Dict.fromList
