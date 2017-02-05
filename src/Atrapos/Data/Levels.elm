57505c228fc7e9f04624eee24eb308ce8f5cebeb
Yury Martynov <yuri.martynov@gmail.com>
2017-02-05 11:06:03 +0200

    L12

--- src/Atrapos/Data/Levels.elm
+++ src/Atrapos/Data/Levels.elm
@@ -5,13 +5,30 @@ import Atrapos.Data.Model exposing (Levels)
 import Atrapos.Data.L1 as L1
 import Atrapos.Data.L2 as L2
 import Atrapos.Data.L3 as L3
-
+import Atrapos.Data.L4 as L4
+import Atrapos.Data.L5 as L5
+import Atrapos.Data.L6 as L6
+import Atrapos.Data.L7 as L7
+import Atrapos.Data.L8 as L8
+import Atrapos.Data.L9 as L9
+import Atrapos.Data.L10 as L10
+import Atrapos.Data.L11 as L11
+import Atrapos.Data.L12 as L12
 
 model : Levels
 model =
     [ L1.model
     , L2.model
     , L3.model
+    , L4.model
+    , L5.model
+    , L6.model
+    , L7.model
+    , L8.model
+    , L9.model
+    , L10.model
+    , L11.model
+    , L12.model
     ]
         |> List.indexedMap (,)
         |> Dict.fromList
