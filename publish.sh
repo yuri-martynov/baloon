00e89b7993ad0e2469d1f4d63457d3f8600019e5
Yury Martynov <yuri.martynov@gmail.com>
2017-02-12 20:36:33 +0200

    split css

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 cp src/atrapos/index.html docs
 cp src/atrapos/main.js docs
-cp src/atrapos/style.css docs
+cp src/atrapos/*.css docs
 cp src/atrapos/*.png docs
 cp src/atrapos/*.svg docs
 
 cp src/atrapos/main.js src/Atrapos.Cordova/www/js
-cp src/atrapos/style.css src/Atrapos.Cordova/www
+cp src/atrapos/*.css src/Atrapos.Cordova/www
 cp src/atrapos/*.png src/Atrapos.Cordova/www
 cp src/atrapos/*.svg src/Atrapos.Cordova/www
 
